6cb92b975d0312a3161b8023cddf21ddf9bb4cbd
laura <laura@codeup.com>
2023-02-06 11:50:26 -0600

    initial commit

---
+++ src/main/java/com/codeup/codeupjavacotestcapstone/CodeupJavacoTestCapstoneApplication.java
@@ -0,0 +1,13 @@
+package com.codeup.codeupjavacotestcapstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeupJavacoTestCapstoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeupJavacoTestCapstoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codeup/codeupjavacotestcapstone/CodeupJavacoTestCapstoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeup.codeupjavacotestcapstone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeupJavacoTestCapstoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
